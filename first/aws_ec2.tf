5c5c34d9ad3ab676a0f152be4404f056df00a82f
seitamuro <seitamuro0210@gmail.com>
2022-11-19 23:35:53 +0900

    Simple Web Server

---
+++ first/aws_ec2.tf
@@ -0,0 +1,14 @@
+# EC2インスタンスの作成
+resource "aws_instance" "sample_web_server" {
+    ami = "ami-072bfb8ae2c884cc4" # AWSコンソール上で確認すること
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.sample_subnet.id
+    vpc_security_group_ids = [aws_security_group.sample_sg.id]
+
+    user_data = <<EOF
+    #!/bin/bash
+    sudo yum install -y httpd
+    sudo systemctl start httpd
+    sudo systemctl enable httpd
+    EOF
+}
